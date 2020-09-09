e40bb47dffcc0705e678c24b3a9efbb520b9a2e5
darkcount2011 <pedrotje.b@gmail.com>
2020-09-09 11:03:18 +0200

    final

---
+++ javascript.js
@@ -0,0 +1,10 @@
+(function () { 
+    var grade = document.getElementById("grade");
+    if ( grade == "--") {
+      grade.style.backgroundColor = '#660000';
+    } 
+    else  
+    {
+      grade.style.backgroundColor = '#FF6C60';
+    }
+  })(); 
