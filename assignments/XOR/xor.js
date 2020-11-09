e84ea1bbb00bde9abafce110485cf67a8e031e69
darkcount2011 <pedrotje.b@gmail.com>
2020-11-09 09:59:43 +0100

    initial commit

---
+++ assignments/XOR/xor.js
@@ -0,0 +1,9 @@
+let first = 5; // May be changed
+let second = 10; // May be changed
+
+//XOR is made, because first is not equal to second, and if they are equal XOR is not made
+if (first != second) {
+  console.log("We made XOR");
+} else {
+  console.log("We didn't make XOR");
+}
